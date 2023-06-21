b75f4f8068ef657d0af91c6ec5b01b20488195a1
Zawar_Mughal <zawar.shafiq@devigital.com>
2023-06-21 16:53:41 +0500

    Navbar changes

--- remote/src/components/NavBar.jsx
+++ remote/src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
     <>
       <Link to="/remote-home">Remote Home</Link> &nbsp;
       <Link to="/remote-about">Remote About</Link> &nbsp;
-      <Link to="/remote-contact">Remote Contact</Link>
+      <Link to="/remote-contact">Remote Contact123</Link>
       <Outlet />
     </>
   );
